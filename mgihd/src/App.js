d36931fc680bc47670336fa21e5a433ef17e85a4
Zorex Salvo <zorexsalvo@gmail.com>
2019-08-13 22:09:44 +0800

    Refactor app component

--- mgihd/src/App.js
+++ mgihd/src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PageHeaderComponent from './components/PageHeaderComponent';
 import SearchComponent from './components/SearchComponent';
 import ContentListingComponent from './components/ContentListingComponent';
+import { inject, observer } from 'mobx-react';
 import { Row, Col } from 'antd';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <PageHeaderComponent />
-      <Row>
-        <Col span={16} offset={4}>
-          <SearchComponent />
-        </Col>
-      </Row>
-      <Row>
-        <Col span={18} offset={3}>
-          <ContentListingComponent />
-        </Col>
-      </Row>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <PageHeaderComponent />
+        <Row>
+          <Col span={16} offset={4}>
+            <SearchComponent />
+          </Col>
+        </Row>
+        <Row>
+          <Col span={18} offset={3}>
+            <ContentListingComponent store={this.props.store.content} />
+          </Col>
+        </Row>
+      </div>
+    );
+  }
 }
 
-export default App;
+export default inject('store')(observer(App));
