08bbbe727084e25f663284118cf0e8ee9cf43845
Zorex Salvo <zorexsalvo@gmail.com>
2019-08-13 22:10:14 +0800

    Setup store/state provider

--- mgihd/src/index.js
+++ mgihd/src/index.js
@@ -3,9 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { Provider } from 'mobx-react';
+import RootStore from './store/RootStore';
 import 'antd/dist/antd.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const rootStore = new RootStore();
+
+ReactDOM.render(
+  <Provider store={rootStore}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
