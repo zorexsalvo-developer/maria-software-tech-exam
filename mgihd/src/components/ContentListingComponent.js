8fab46c567a2bb3a9cbff79759c0bb439db9bc87
Zorex Salvo <zorexsalvo@gmail.com>
2019-08-13 22:28:37 +0800

    Refactor: Move description lists to another component

--- mgihd/src/components/ContentListingComponent.js
+++ mgihd/src/components/ContentListingComponent.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { observer } from 'mobx-react';
-import { Table, List, Typography, Descriptions, Tag } from 'antd';
+import { Table, List, Typography, Tag } from 'antd';
+import DescriptionListComponent from './DescriptionListComponent';
 import './ContentListingComponent.scss';
 
 const { Text } = Typography;
@@ -91,33 +92,7 @@ class ContenListingComponent extends Component {
           dataSource={this.props.store.results}
           loading={this.props.store.ui.loading}
           expandedRowRender={record => (
-            <Descriptions title="Drugs Information" layout="vertical" bordered>
-              {record.indications_and_usage ? (
-                <Descriptions.Item label="Indications And Usage">
-                  {record.indications_and_usage}
-                </Descriptions.Item>
-              ) : null}
-              {record.warnings ? (
-                <Descriptions.Item label="Warnings">
-                  {record.warnings}
-                </Descriptions.Item>
-              ) : null}
-              {record.active_ingredient ? (
-                <Descriptions.Item label="Active Ingredients">
-                  {record.active_ingredient}
-                </Descriptions.Item>
-              ) : null}
-              {record.inactive_ingredient ? (
-                <Descriptions.Item label="Inactive Ingredients">
-                  {record.inactive_ingredient}
-                </Descriptions.Item>
-              ) : null}
-              {record.storage_and_handling ? (
-                <Descriptions.Item label="Storage and Handling">
-                  {record.storage_and_handling}
-                </Descriptions.Item>
-              ) : null}
-            </Descriptions>
+            <DescriptionListComponent record={record} />
           )}
         />
       </div>
---
+++ mgihd/src/components/DescriptionListComponent.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import { Descriptions } from 'antd';
+
+export default class DescriptionListComponent extends Component {
+  render() {
+    const { record } = this.props;
+    return (
+      <Descriptions title="Drugs Information" layout="vertical" bordered>
+        {record.indications_and_usage ? (
+          <Descriptions.Item label="Indications And Usage">
+            {record.indications_and_usage}
+          </Descriptions.Item>
+        ) : null}
+        {record.warnings ? (
+          <Descriptions.Item label="Warnings">
+            {record.warnings}
+          </Descriptions.Item>
+        ) : null}
+        {record.active_ingredient ? (
+          <Descriptions.Item label="Active Ingredients">
+            {record.active_ingredient}
+          </Descriptions.Item>
+        ) : null}
+        {record.inactive_ingredient ? (
+          <Descriptions.Item label="Inactive Ingredients">
+            {record.inactive_ingredient}
+          </Descriptions.Item>
+        ) : null}
+        {record.storage_and_handling ? (
+          <Descriptions.Item label="Storage and Handling">
+            {record.storage_and_handling}
+          </Descriptions.Item>
+        ) : null}
+      </Descriptions>
+    );
+  }
+}
