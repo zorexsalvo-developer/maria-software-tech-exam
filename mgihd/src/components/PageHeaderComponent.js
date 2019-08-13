d6f85e5194de83f0fd96c241af6291686df69090
Zorex Salvo <zorexsalvo@gmail.com>
2019-08-13 18:03:37 +0800

    Add Permanent Marker fontstyle

--- mgihd/src/components/PageHeaderComponent.js
+++ mgihd/src/components/PageHeaderComponent.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import { Menu, Icon } from 'antd';
+import './PageHeaderComponent.scss';
 
 class PageHeaderComponent extends Component {
   render() {
     return (
       <Menu mode="horizontal">
-        <Menu.Item key="email">
+        <Menu.Item key="email" className="PageHeader">
           <Icon type="medicine-box" />
           My Gad, I Hate Drugs!
         </Menu.Item>
