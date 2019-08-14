5bdd5bacb1b3dcf227d4ee15b049d9a574253337
Zorex Salvo <zorexsalvo@gmail.com>
2019-08-14 14:28:44 +0800

    Implement add to cart endpoint

--- zorex-salvo/data-driven/api/urls.py
+++ zorex-salvo/data-driven/api/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
-from .views import ListPlanAPIView, CreateCartAPIView, RetrieveCartAPIView
+from .views import ListPlanAPIView, CreateCartAPIView, RetrieveCartAPIView, AddToCartAPIView
 
 urlpatterns = [
     path('plans/', ListPlanAPIView.as_view(), name='list_plan'),
     path('carts/', CreateCartAPIView.as_view(), name='create_cart'),
+    path('carts/<identifier>/add',
+         AddToCartAPIView.as_view(),
+         name='add_to_cart'),
     path('carts/<identifier>/',
          RetrieveCartAPIView.as_view(),
-         name='retrieve_cart')
+         name='retrieve_cart'),
 ]
--- zorex-salvo/data-driven/api/views.py
+++ zorex-salvo/data-driven/api/views.py
@@ -1,6 +1,9 @@
 from rest_framework.generics import ListAPIView, CreateAPIView, RetrieveAPIView
-from plan.models import Plan
-from cart.models import Cart
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from plan.models import Plan, Term
+from cart.models import Cart, Item
 from .serializers import ListPlanSerializer, CreateCartSerializer
 
 
@@ -18,3 +21,41 @@ class RetrieveCartAPIView(RetrieveAPIView):
     queryset = Cart.objects.all()
     serializer_class = CreateCartSerializer
     lookup_field = 'identifier'
+
+
+class AddToCartAPIView(APIView):
+    def put(self, request, identifier):
+        data = request.data
+        cart = Cart.objects.filter(status=Cart.STATUS.pending,
+                                   identifier=identifier)
+
+        if (cart):
+            cart = cart.first()
+            for item in data:
+                plan = Plan.objects.get(identifier=item['plan'])
+                terms = Term.objects.filter(plan=plan,
+                                            term=item['payment_term'])
+
+                if not terms:
+                    return Response(
+                        {
+                            'message':
+                            f"Payment term '{item['payment_term']}' is not available for {plan.name}"
+                        },
+                        status=status.HTTP_400_BAD_REQUEST)
+
+                item['cart_id'] = cart.id
+                item['plan'] = plan
+                Item.objects.create(**item)
+                cart.total_amount = cart.total_amount + (terms.first().amount *
+                                                         item['quantity'])
+                cart.save()
+
+            return Response(CreateCartSerializer(cart).data)
+        else:
+            return Response(
+                {
+                    'message':
+                    'Cart not found. Invalid cart identifier or cart is already paid. '
+                },
+                status=status.HTTP_404_NOT_FOUND)
