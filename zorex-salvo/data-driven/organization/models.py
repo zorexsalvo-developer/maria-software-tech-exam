63940a329be50fcd8dbe218ec2af6f4332e3568a
Zorex Salvo <zorexsalvo@gmail.com>
2019-08-14 10:31:24 +0800

    Initialize organization app; create model

---
+++ zorex-salvo/data-driven/organization/models.py
@@ -0,0 +1,8 @@
+import uuid
+from django.db import models
+from model_utils.models import TimeStampedModel
+
+
+class Organization(TimeStampedModel):
+    identifier = models.UUIDField(default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=255)
