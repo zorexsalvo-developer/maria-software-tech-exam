a91d3e41b81528cbe73485bca9aa82789f397004
Zorex Salvo <zorexsalvo@gmail.com>
2019-08-14 11:12:10 +0800

    Refactor plan model admin

--- zorex-salvo/data-driven/plan/admin.py
+++ zorex-salvo/data-driven/plan/admin.py
@@ -1,5 +1,15 @@
 from django.contrib import admin
 from .models import Plan, Term
 
-admin.site.register(Plan)
+
+class TermsInline(admin.TabularInline):
+    extra = 1
+    model = Term
+
+
+class PlanModelAdmin(admin.ModelAdmin):
+    inlines = [TermsInline]
+
+
+admin.site.register(Plan, PlanModelAdmin)
 admin.site.register(Term)
