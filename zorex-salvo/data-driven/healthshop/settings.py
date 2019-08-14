32b91a4cbcc5087e1089f3c3b2ce497bc177540c
Zorex Salvo <zorexsalvo@gmail.com>
2019-08-14 15:45:25 +0800

    Set JSON Renderer as default renderer class

--- zorex-salvo/data-driven/healthshop/settings.py
+++ zorex-salvo/data-driven/healthshop/settings.py
@@ -118,3 +118,9 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 STATIC_URL = '/static/'
+
+REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.JSONRenderer',
+    ]
+}
