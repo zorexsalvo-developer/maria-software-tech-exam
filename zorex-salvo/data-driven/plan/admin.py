90e783f2a82478f499f86df996291a5c1b5931ec
Zorex Salvo <zorexsalvo@gmail.com>
2019-08-14 10:54:37 +0800

    Create model for payment terms

--- zorex-salvo/data-driven/plan/admin.py
+++ zorex-salvo/data-driven/plan/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import Plan
+from .models import Plan, Term
 
 admin.site.register(Plan)
+admin.site.register(Term)
--- zorex-salvo/data-driven/plan/models.py
+++ zorex-salvo/data-driven/plan/models.py
@@ -1,6 +1,7 @@
 import uuid
 from django.db import models
 from model_utils.models import TimeStampedModel
+from model_utils import Choices
 from organization.models import Organization
 
 
@@ -10,3 +11,15 @@ class Plan(TimeStampedModel):
     hmo = models.ForeignKey(Organization,
                             on_delete=models.CASCADE,
                             related_name='health_plans')
+
+
+class Term(TimeStampedModel):
+    PAYMENT_TERMS = Choices(('monthly', 'Monthly'), ('quarterly', 'Quarterly'),
+                            ('annually', 'Annually'))
+
+    plan = models.ForeignKey('Plan',
+                             on_delete=models.CASCADE,
+                             related_name='payment_terms')
+    identifier = models.UUIDField(default=uuid.uuid4, editable=False)
+    term = models.CharField(max_length=255, choices=PAYMENT_TERMS)
+    amount = models.IntegerField(help_text="in cents")
